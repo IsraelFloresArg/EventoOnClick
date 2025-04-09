2b4391fe5332d0cf135311b948aa67466b42181c
IsraelFloresArg <israelfloreslaboral@gmail.com>
2025-04-09 17:56:16 -0300

    actualizado

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("Hola desde JS");
+
+let textoBoton = document.querySelector(".login");
+console.log(textoBoton.textContent);
+textoBoton.addEventListener("click", function(){
+    textoBoton.textContent = ("Cerrar sesión");
+})
+
+//function sesion(){
+//    textoBoton = document.querySelector("login");
+//    textoBoton.textContent =("Cerrar sesión");
+//}
+
+function alarma(){
+    alert("Te gusto");
+}
+
+let borrar = document.querySelector(".botonLargo");
+    borrar.addEventListener("click", function(borrarBoton){
+        borrarBoton.target.remove();
+    })
+
+//function borrar(elemento){
+//    elemento.target.remove();
+//}
